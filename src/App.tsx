1c5ecb49a4eada6b538a19bd37270eb8248b5ae8
anuragchhetri <karanchheti@gmail.com>
2024-05-20 01:41:47 +0530

    added comments

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,11 @@ import { DarkModeProvider } from './context/DarkModeContext'
 function App() {
 
   return (
+    // dark mode gloabal provider to set dark mode / light mode across the application
     <DarkModeProvider>
+      {/* SplashScreen with 5 seconds animation */}
       <SplashScreen />
+      {/* main landing page after SplashScreen */}
       <Landing />
     </DarkModeProvider>
   )
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,10 +1,9 @@
 import styles from './Banner.module.css'
 const Banner = () => {
   return (
+    // banner container
     <div className={styles.banner_container}>
       <span>Event : Oasis Bus tour , JLN Stadium , Delhi * Collection Live : Meta Lives , live on astrix </span>
-
-      {/* <span>csa : Oasis Bus tour , JLN Stadium , Delhi * Collection Live : Meta Lives , live on astrix </span> */}
     </div>
   )
 }
