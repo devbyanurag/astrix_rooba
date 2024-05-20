cf462651c7d8ac57c63c05f781674456cdc35860
anuragchhetri <karanchheti@gmail.com>
2024-05-20 10:31:58 +0530

    changed banner duration

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -4,6 +4,7 @@ const Banner = () => {
     // banner container
     <div className={styles.banner_container}>
       <span>Event : Oasis Bus tour , JLN Stadium , Delhi * Collection Live : Meta Lives , live on astrix </span>
+      
     </div>
   )
 }
